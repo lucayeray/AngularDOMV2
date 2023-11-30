ab413f21818c4ea99fe914e3a85243f47bb53019
JoelPigem <joel.pigem@institutvidreres.cat>
2023-11-30 11:25:29 +0100

    final

---
+++ src/app/component1/component1.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, UpperCasePipe} from '@angular/common';
+// @ts-ignore
+import {yellowSubmarine} from './funcions.js';
+// @ts-ignore
+import {changeSize} from "./funcions.js";
+// @ts-ignore
+import {countWords} from "./funcions.js";
+
+@Component({
+  selector: 'app-component1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './component1.component.html',
+  styleUrl: './component1.component.css'
+})
+export class Component1Component implements OnInit{
+  url: any
+constructor() {
+  // this.url = window.location
+  this.url = document.URL
+
+}
+ngOnInit(){
+  let maj = document.getElementById("ex1")!
+  maj.innerHTML = maj.innerHTML.toUpperCase()
+
+  yellowSubmarine()
+  changeSize()
+  countWords()
+
+  let img = document.getElementById("imatge") as HTMLImageElement
+  img.src = "./assets/gatigos.jpg"
+}
+}
+
